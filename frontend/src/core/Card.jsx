fd31e257fa7929035e09f7f83e89428c928e54f8
Dhrumil Bhut <dhrumilbhut@gmail.com>
2023-04-27 10:20:36 +0530

    api link added

--- frontend/src/core/Card.jsx
+++ frontend/src/core/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { addItemToCart, removeItemFromCart } from "./helper/CartHelper";
+import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
 import ImageHelper from "./helper/ImageHelper";
 
 const Card = ({
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,4 +4,6 @@ import Home from "./core/Home";
 import Paths from "./Paths";
 import "./styles.css";
 
+console.log(import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<Paths />);
